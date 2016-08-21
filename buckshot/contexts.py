61be48aedfc70f6361542bbb0b7a96d13c5ce4d9
Bryan Worrell <bworrell@insightsquared.com>
2016-08-21 14:27:06 -0400

    Added docstrings to contexts module.

--- buckshot/contexts.py
+++ buckshot/contexts.py
@@ -20,6 +20,18 @@ LOG = logging.getLogger(__name__)
 
 
 class distributed(object):
+    """Context manager that distributes an input worker function across
+    multiple subprocesses.
+
+    The object returned from ``with`` accepts an iterable object. Each item
+    in the iterable object must match the input worker function *args.
+
+    Args:
+        func: A callable object to be distributed across subprocesses.
+        processes (int): The number of subprocesses to spawn. If not
+            provided, the number of CPUs on the system will be used.
+    """
+
     def __init__(self, func, processes=None):
         self._func = func
         self._size = processes or multiprocessing.cpu_count()
@@ -28,10 +40,13 @@ class distributed(object):
         self._out_queue = None
 
     def __len__(self):
+        """Return the number of processes registered."""
         return len(self._process_map)
 
     @logutils.tracelog(LOG)
     def __enter__(self):
+        """Initialize our subprocesses and return self."""
+
         self._process_map = {}
         self._in_queue = multiprocessing.Queue(maxsize=self._size)
         self._out_queue = multiprocessing.Queue()
@@ -55,10 +70,22 @@ class distributed(object):
 
     @logutils.tracelog(LOG)
     def __exit__(self, ex_type, ex_value, traceback):
+        """Kill any spawned subprocesses."""
         self._kill_subprocesses()
 
     @logutils.tracelog(LOG)
     def __call__(self, iterable):
+        """Map each item in the input `iterable` to our worker subprocesses.
+        When results become availble, yield them to the caller.
+
+        Args:
+            iterable: An iterable collection of *args to be passed to the
+                worker function.
+
+        Yields:
+            Results from the worker function. If a subprocess error occurs,
+            the result value will be an instance of errors.SubprocessError.
+        """
         send_counter = itertools.count(1)
         recv_counter = itertools.count(1)
         num_sent = num_recv = 0
@@ -83,12 +110,18 @@ class distributed(object):
             num_recv = next(recv_counter)
 
     def _lookup_process(self, pid):
+        """Get the Process object associated with the pid."""
         return self._process_map[pid]
 
     def _register_process(self, process):
+        """Register the process in the internal process map."""
+        assert process.pid not in self._process_map
         self._process_map[process.pid] = process
 
     def _unregister_process(self, pid):
+        """Attempt to remove the processes associated with the pid from
+        the internal process map.
+        """
         try:
             del self._process_map[pid]
         except KeyError:
@@ -96,9 +129,12 @@ class distributed(object):
 
     def _kill_subprocesses(self, *args, **kwargs):
         """Handle any SIGCHLD signals by attempting to kill all spawned
-        processes and raising a RuntimeError.
+        processes.
 
         This removes all processes from the internal process map.
+
+        Raises:
+            RuntimeError: If a SIGCHLD signal was caught during processing.
         """
         signal.signal(signal.SIGCHLD, signal.SIG_IGN)
 
@@ -106,6 +142,11 @@ class distributed(object):
             LOG.debug("Killing subprocess %s.", pid)
             os.kill(pid, signal.SIGTERM)
             self._unregister_process(pid)
+
+        # FIXME: Make this better.
+        # If a SIGCHLD signal occurred during processes, raise a RuntimeError.
+        if args or kwargs:
+            raise RuntimeError("SIGCHLD occurred")
 
 
 
