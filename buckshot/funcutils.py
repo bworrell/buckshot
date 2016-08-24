0ff9744e8f043a5de2f3b547e655261754e705d6
Bryan Worrell <bworrell@mitre.org>
2016-08-24 01:28:25 -0400

    Added funcutils module. Needs docs!

---
+++ buckshot/funcutils.py
@@ -0,0 +1,60 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import logging
+import functools
+
+LOG = logging.getLogger(__name__)
+
+
+def assert_unlocked(obj):
+    if getattr(obj, "__is_locked", False):
+        raise RuntimeError("Item is locked.")
+
+
+def lock_function(func):
+    @functools.wraps(func)
+    def inner(*args, **kwargs):
+        assert_unlocked(func)
+        func.__is_locked = True
+        try:
+            retval = func(*args, **kwargs)
+        except:
+            func.__is_locked = False
+            raise
+        return retval
+    return inner
+
+
+def unlock_function(func):
+    @functools.wraps(func)
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        finally:
+            func.__is_locked = False
+    return inner
+
+
+def lock_instance(func):
+    @functools.wraps(func)
+    def inner(self, *args, **kwargs):
+        assert_unlocked(func)
+        self.__is_locked = True
+        try:
+            retval = func(self, *args, **kwargs)
+        except:
+            self.__is_locked = False
+            raise
+        return retval
+    return inner
+
+
+def unlock_instance(func):
+    @functools.wraps(func)
+    def inner(self, *args, **kwargs):
+        try:
+            return func(self, *args, **kwargs)
+        finally:
+            self.__is_locked = False
+    return inner
