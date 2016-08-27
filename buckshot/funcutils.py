6e10b090cca351428c3aa3c97bc63fe59c505c3f
Bryan Worrell <bworrell@insightsquared.com>
2016-08-27 14:44:35 -0400

    Added docstrings to funcutils.

--- buckshot/funcutils.py
+++ buckshot/funcutils.py
@@ -8,11 +8,17 @@ LOG = logging.getLogger(__name__)
 
 
 def assert_unlocked(obj):
+    """Assert that an input object has a falsey __is_locked value or
+    no __is_locked value set.
+    """
     if getattr(obj, "__is_locked", False):
         raise RuntimeError("Item is locked.")
 
 
 def lock_instance(func):
+    """Set an __is_locked=True attr on the method owner. Unlock if an
+    error occurs during execution of the method.
+    """
     @functools.wraps(func)
     def inner(self, *args, **kwargs):
         assert_unlocked(func)
@@ -27,6 +33,9 @@ def lock_instance(func):
 
 
 def unlock_instance(func):
+    """Set an __is_locked=False attr on the method owner after finishing
+    execution of the method.
+    """
     @functools.wraps(func)
     def inner(self, *args, **kwargs):
         try:
