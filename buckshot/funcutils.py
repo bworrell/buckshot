39f4e4f0b3360f7736cbc8ffb138e879437bf90e
Bryan Worrell <bworrell@insightsquared.com>
2016-08-27 22:16:48 -0400

    Added is_generator() utility function.

--- buckshot/funcutils.py
+++ buckshot/funcutils.py
@@ -1,10 +1,16 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+import inspect
 import logging
 import functools
 
 LOG = logging.getLogger(__name__)
+
+
+def is_generator(func):
+    """Return True if `func` is a generator function."""
+    return inspect.isgeneratorfunction(func)
 
 
 def assert_unlocked(obj):
