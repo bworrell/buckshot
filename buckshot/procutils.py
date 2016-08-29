56db25079eff2208f14acce1c8fb2c9817f66699
Bryan Worrell <bworrell@mitre.org>
2016-08-28 23:18:32 -0400

    Added more docs to procutils.

--- buckshot/procutils.py
+++ buckshot/procutils.py
@@ -11,9 +11,14 @@ import functools
 
 LOG = logging.getLogger(__name__)
 
+
 def suppress(sig, default=True):
-    """Suppressthe input signal during the execution of the wrapped
+    """Suppress the input signal during the execution of the wrapped
     function. Upon exit, return the signal handler to its default value.
+
+    Warning:
+        This modifies the signal handler globally for a process, so this is
+        basically useless.
     """
     def decorator(func):
         @functools.wraps(func)
