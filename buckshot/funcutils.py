8c8f332cb9a249a9d7b5494a7aad08ad6c45a953
Bryan Worrell <bworrell@insightsquared.com>
2016-10-22 12:49:32 -0400

    Removed distributed_memoize from funcutils module.

--- buckshot/funcutils.py
+++ buckshot/funcutils.py
@@ -3,9 +3,6 @@ from __future__ import unicode_literals
 
 import inspect
 import logging
-import functools
-import multiprocessing
-
 
 LOG = logging.getLogger(__name__)
 
@@ -13,32 +10,6 @@ LOG = logging.getLogger(__name__)
 def is_generator(func):
     """Return True if `func` is a generator function."""
     return inspect.isgeneratorfunction(func)
-
-
-def distributed_memoize(func):
-    """A memoization decorator that works across multiple processes.
-
-    Warning:
-        This is a bad design that will almost certainly cause memory
-        issues.
-    """
-    manager = multiprocessing.Manager()
-    cache = manager.dict()
-
-    def keyfunc(args):
-        return unicode(args)
-
-    @functools.wraps(func)
-    def inner(*args):
-        key = keyfunc(args)
-
-        try:
-            retval = cache[key]
-        except KeyError:
-            cache[key] = retval = func(*args)
-        return retval
-
-    return inner
 
 
 def patch_recursion(func):
